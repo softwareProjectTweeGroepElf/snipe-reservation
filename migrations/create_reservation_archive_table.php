4f3d23d4054b38b25698ea6d2cc5b2d7e7a1b58c
Deleu Tanguy <tanguy.deleu@student.ehb.be>
2016-11-17 23:57:50 +0100

    Created CreateReservationArchiveTable migration Changed CreateReservationRequestsTable to no longer include the status of the Reservation

--- migrations/create_reservation_archive_table.php
+++ migrations/create_reservation_archive_table.php
@@ -4,20 +4,23 @@
  * Created by PhpStorm.
  * User: Tanguy
  * Date: 17/11/2016
- * Time: 23:11
+ * Time: 23:53
  */
 
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReservationRequestsTable extends Migration
+class CreateReservationArchiveTable extends Migration
 {
 
     public function up()
     {
-        Schema::create('reservation_requests', function(Blueprint $table) {
+        Schema::create('reservation_archive', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('asset_id');
+            $table->dateTime('from');
+            $table->dateTime('until');
+            $table->enum('status', [ 'ACCEPTED', 'DENIED' ] );
             $table->timestamps();
         });
     }
