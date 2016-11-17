0adabaef85e168ca4fcd029fd9cbf238f27adfc2
Deleu Tanguy <tanguy.deleu@student.ehb.be>
2016-11-17 23:09:26 +0100

    Added CreateReservationTable migration

---
+++ migrations/create_reservation_table.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Tanguy
+ * Date: 17/11/2016
+ * Time: 23:01
+ */
+use \Illuminate\Database\Migrations\Migration;
+
+class CreateReservationTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('reservation_assets', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('asset_id');
+            $table->dateTime('from');
+            $table->dateTime('until');
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('reservation_assets');
+    }
+}
