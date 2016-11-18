d08a46f7cc31df815b953db38a4d6ec517f562ab
Deleu Tanguy <tanguy.deleu@student.ehb.be>
2016-11-18 01:41:50 +0100

    Modified ReservationController to make use of RoleUtil's checkUserRole

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -3,18 +3,43 @@
 namespace Reservation\Controllers;
 
 use App\Http\Controllers\Controller;
+use Reservation\Fetchers\ReservationFetcher;
+use Reservation\Util\RoleUtil;
 use App\Models\Asset;
 use App\Models\User;
 
 class ReservationController extends Controller
 {
-	
-	public function getAvailableAssets()
-	{
-		$assets = Asset::select()->whereNotIn('id', function($query) {
-			$query->table('reservation_assets')->select('asset_id')->get();
-		})->get();
+	//private functions
 
-		return $assets;
+
+
+	//public functions
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+	public function getReservation()
+	{
+		return view('reservation')->with('assets', ReservationFetcher::getAvailableAssets());
+	}
+
+	public function getReservationRequests()
+	{
+		if (RoleUtil::checkUserRole(2))
+			return redirect()->back();
+		else
+			return view('reservationrequests')->with('requests', ReservationFetcher::getReservationRequests());
+	}
+
+	public function getLeasingService()
+	{
+		if (RoleUtil::checkUserRole(3))
+			return redirect()->back();
+		else
+		return view('leasingservice')->with([
+			['leasedAssets', ReservationFetcher::getLeasedAssets()],
+		]); // will add more variables to send
 	}
 }
