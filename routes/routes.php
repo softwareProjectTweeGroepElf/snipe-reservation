ab8302a3e766c6cdf7b9c891d7e6088689d01df2
Sam-PC\Sam <sam.van.roy@student.ehb.be>
2016-11-20 10:02:11 +0100

    adjustments in ReservationServiceProvider + view teacher

--- routes/routes.php
+++ routes/routes.php
@@ -1,5 +1,9 @@
 <?php
-Route::group([ 'prefix'  => 'package' ], function() {
+/*Route::group([ 'prefix'  => 'package' ], function() {
 	//Route::get('/home', 'groepelf\reservatie\Http\ReservatieController@getIndex');
-	
+
+});*/
+
+Route::get('reservations', function(){
+    return view('teacher');
 });
