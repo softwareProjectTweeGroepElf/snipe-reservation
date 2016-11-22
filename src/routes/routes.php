a6a6fa30552da9a98d1eef36811986cdcfcbc278
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2016-11-22 23:24:52 +0100

    test

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,7 +1,7 @@
 <?php
 Route::group([ 'prefix'  => 'package' ], function() {
 	Route::get('/home', function(){return "it works";});
-	Route::get('/home2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
+	//Route::get('/home2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
 
 
 	
