90e230a32c3728ca93b325e284789bffe4275e78
Sam-PC\Sam <sam.van.roy@student.ehb.be>
2016-11-22 10:58:36 +0100

    maken van functie sendReminderMailToUsers

--- controllers/ReservationController.php
+++ controllers/ReservationController.php
@@ -83,7 +83,55 @@ class ReservationController extends Controller
             ->whereDate('until', $today)
             ->get();
         return $reservations;
+    }
+
+    public function sendReminderMailToUsers(){
+
+        $reservations = $this->getAllEndDateReservations();
+        foreach ($reservations as $reservation)
+        {
+            $user_id = $reservation->user_id;
+            $user = User::find($user_id);
+            $to = $user->email;
+            $subject = "Automatic reminder";
+            $message = $user->name + "your Reservations ends tomorrow!";
+            mail($to, $subject, $message);
+        }
+        /*
+        //require_once 'lib/swift_required.php';
+
+        // Create the Transport
+        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 25)
+            ->setUsername('Admin')
+            ->setPassword('Admin')
+        ;
 
 
+        You could alternatively use a different transport such as Sendmail or Mail:
+
+        // Sendmail
+        $transport = Swift_SendmailTransport::newInstance('/usr/sbin/sendmail -bs');
+
+        // Mail
+        $transport = Swift_MailTransport::newInstance();
+
+
+        //Create the email body message
+        $data =
+
+        // Create the Mailer using your created Transport
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        // Create a message
+        $message = Swift_Message::newInstance('Web Lead')
+            ->setFrom(array('sam.van.roy@student.ehb.be' => 'Sam Van Roy'))
+            ->setTo(array( 'other@domain.org' => 'A name'))
+            ->setSubject('Automatic email from sam.van.roy@student.ehb.be')
+            ->setBody()
+        ;
+
+        // Send the message
+        $result = $mailer->send($message);
+        */
     }
 }
