20df17f2a442215dd5492bd0bff6d8444b320dd3
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2016-11-22 23:41:28 +0100

    Hopefully working version, getting weird errors with composer require?

---
+++ src/providers/ReservationServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+//namespace sp2gr11\reservation;
+
+use Illuminate\Support\ServiceProvider;
+
+class ReservationServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+		$this->app->bind('Reservation', function($app){
+			return new Reservation();
+		});
+	}
+	public function boot()
+	{
+		if (! $this->app->routesAreCached()) {
+			require __DIR__ . '../routes/routes.php';
+		}
+
+		$this->loadMigrationsFrom(__DIR__ . '../migrations');
+        $this->loadViewsFrom(__DIR__ . '../views', 'Reservation');
+
+		$this->publishes([
+			__DIR__ . '../config/reservation.php' => config_path('reservation.php'),
+		]);
+	}
+
+
+}
---
+++ src/routes/routes.php
@@ -0,0 +1,8 @@
+<?php
+Route::group([ 'prefix'  => 'package' ], function() {
+	Route::get('/home', function(){return "it works";});
+	Route::get('/home2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
+
+
+	
+});
