a8e79262d41370f918c5a5d42813b901bf25d9d4
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2016-11-24 11:55:59 +0100

    Renamed the migrations & tested if They work by manually copying them to the database/migrations folder with succes. Also changed some small thingy within every migration. (Also removed routes & serviceprovider that wasn't being used) For now people are going to have to copy the migrations to the correct folder ...

--- src/Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-Route::group([ 'prefix'  => 'package' ], function() {
-	Route::get('/home', 'groepelf\reservatie\Http\ReservatieController@getIndex');
-	Route::get('/home2', 'groepelf\reservatie\Http\ReservatieController@getIndex2');
-
-});
--- src/migrations/2016_11_20_132344_create_reservation_requests_table.php
+++ src/migrations/2016_11_20_132344_create_reservation_requests_table.php
@@ -4,25 +4,27 @@
  * Created by PhpStorm.
  * User: Tanguy
  * Date: 17/11/2016
- * Time: 23:01
+ * Time: 23:11
  */
+
 use Illuminate\Database\Migrations\Migration;
 
-class CreateReservationTable extends Migration
+class CreateReservationRequestsTable extends Migration
 {
+
     public function up()
     {
-        Schema::create('reservation_assets', function(Blueprint $table) {
+        Schema::create('reservation_requests', function($table) {
             $table->increments('id');
             $table->integer('user_id');
             $table->integer('asset_id');
-            $table->dateTime('from');
-            $table->dateTime('until');
+            $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('reservation_assets');
+        Schema::dropIfExists('reservation_requests');
     }
+
 }
