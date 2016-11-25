9e70ec206c2b49878b058aab2760d8c7045356b5
TOM-PC2\Tom-PC-School <tom.saccasyn@student.ehb.be>
2016-11-25 18:44:03 +0100

    app en scr zijn door deze commit verwijderd

--- src/routes/routes.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::group([ 'prefix'  => 'package' ], function() {
-	Route::get('/home', function(){return "it works";});
-	Route::get('/home2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
-
-
-	
-});
