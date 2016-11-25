474a9e19e8ec09d7ab1fa4c6f520aba03a2c2898
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2016-11-25 02:08:01 +0100

    Added a option list & added some very nice & crisp css (with help of my mate Robbe Van Hemelryck). Added some nice & Crisp  Ajax & JQuery. Made my controller functions cleaner & easier to understand.

--- src/controllers/ReservationController.php
+++ src/controllers/ReservationController.php
@@ -7,16 +7,9 @@ use Reservation\Fetchers\ReservationFetcher;
 use Reservation\Util\RoleUtil;
 use App\Models\Asset;
 use App\Models\User;
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
 use Illuminate\Support\Facades\DB;
-
-
-//use Illuminate\Routing\Controller;
-
-
 
 class ReservationController extends Controller
 {
@@ -56,49 +49,34 @@ class ReservationController extends Controller
 
 	public function postReservationRequest(Request $request)
 	{
-		///__________________DE INSERT_____________________________________
+	///__________________DE INSERT_____________________________________
 		if(DB::table('reservation_requests')->insert([
 	        'user_id'=> Auth::id(),
-    	    'asset_id' => $request->input('asset_id')   
-        ])){$worked = true;}else{$worked = false;}
-   		
-   		//___________________DATA OPHALEN___________________________________
-
-
-		$var =  Asset::all();
-		for ($i=0; $i < sizeof($var); $i++) { 
-			if (!$var[$i]["assigned_to"] == null){
-				 unset($var[$i]);
-			}
+    	    'asset_id' => $request->asset_id 
+        ])){
+			$worked = true;
 		}
-		
-		$var3 = DB::table('reservation_requests')->get();;
-		
-		for ($i=0; $i <sizeof($var3); $i++) { 
-		
-			$id = $var3[$i]->user_id;
-			$Aid = $var3[$i]->asset_id;
-		
-    		$var3[$i]->name = User::where('id', $id)->get()[0]["first_name"]." ".User::where('id', $id)->get()[0]["last_name"];
-			$var3[$i]->Aname = Asset::where('id', $Aid)->get()[0]["name"];
-			
+		else{
+			$worked = false;
 		}
+
+	///__________________INSERTED VALUE RETURNEN___________________________________
 		
-		//__________________VIEW RETURNEN___________________________________
-		if($worked == true)
-   		{
-   			$var2 = "Your request was submitted succesfully!";
-       		return view('reservation::students')->with('succes', $var2)->with('assets', $var)->with('Rassets',$var3);
+		if($worked == true){
+   			$user_requested_assets = self::getUserAssets();
+			$temp = sizeof($user_requested_assets);
+			$temp = $temp - 1;
+			$inserted_user = $user_requested_assets[$temp];
+
+			$newarray = Array();
+
+			$newarray[0] = $inserted_user->user_id;
+			$newarray[1] = $inserted_user->asset_id;
+			$newarray[2] = $inserted_user->name;
+			$newarray[3] = $inserted_user->Aname;
+			return $newarray;
  	  	}
- 	  	$var2 = "Whoops, something went wrong while submitting your request!";
-		return view('reservation::students')->with('failed', $var2)->with('assets', $var)->with('Rassets',$var3);
-		
-		/*
-		if (reservation_requests::create(Request::All())){
-			return true;
-		}
-		return false;
-		*/
+ 		return false;
 	}
 
 	public function postReservation(Request $request)
@@ -107,7 +85,6 @@ class ReservationController extends Controller
 			return redirect()->back();
 		}
 		else{
-		   // $request->input('user_id') = Auth::id();
 			if(DB::table('reservation_assets')->insert([
 			        'user_id'=> $request->input('user_id'),
 	        	    'asset_id' => $request->input('asset_id'),
@@ -119,42 +96,54 @@ class ReservationController extends Controller
 	        }
 			return false;
 		}
-
-		/*if (reservation_assets::create(Request::All())){
-			return true;
-		}
-		return false; */
 	}
 
 	public function getIndex(){
-
 		return "Reaching the controllers works aswell";
 	}
 
 	public function getStudent(){
-
-		$var =  Asset::all();
-
-		for ($i=0; $i < sizeof($var); $i++) { 
-			if (!$var[$i]["assigned_to"] == null){
-				 unset($var[$i]);
-			}
-		}
-		
-		$var3 = DB::table('reservation_requests')->get();;
-		
-		for ($i=0; $i <sizeof($var3); $i++) { 
-		
-			$id = $var3[$i]->user_id;
-			$Aid = $var3[$i]->asset_id;
-		
-    		$var3[$i]->name = User::where('id', $id)->get()[0]["first_name"]." ".User::where('id', $id)->get()[0]["last_name"];
-			$var3[$i]->Aname = Asset::where('id', $Aid)->get()[0]["name"];
-
-		}
-     
-		return view('reservation::students')->with('assets', $var)->with('Rassets',$var3);
+		return view('reservation::students')->with('assets', self::getFreeAssets())->with('userassets',self::getUserAssets());
 	}
 
+	public function getFreeAssets() {
 
+		$free_assets =  Asset::all();
+
+		for ($i=0; $i < sizeof($free_assets); $i++) { 
+			if (!$free_assets[$i]["assigned_to"] == null){
+				 unset($free_assets[$i]);
+			}
+		}
+	return $free_assets;
+	}
+
+	public function getUserAssets($amount = 0) {
+		
+		$reservation_requests = DB::table('reservation_requests')->get();
+		
+		for ($i=0; $i <sizeof($reservation_requests); $i++) { 
+			
+			$user = User::where('id', $reservation_requests[$i]->user_id)->get()[0];
+			$asset = Asset::where('id', $reservation_requests[$i]->asset_id)->get()[0]["name"];
+
+    		$reservation_requests[$i]->name = $user["first_name"]." ".$user["last_name"];
+			$reservation_requests[$i]->Aname = $asset;
+
+		}
+	return $reservation_requests;
+	}
+
+	public function getAssetIDs(){
+
+		$free_assets =  Asset::all();
+		$free_asset_id = Array();
+
+		for ($i=0; $i <sizeof($free_assets) ; $i++) { 
+
+			array_push($free_asset_id, $free_assets[$i]["id"]);
+
+		}
+	return $free_asset_id;
+	}
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -3,5 +3,6 @@ Route::group([ 'prefix'  => 'package' ], function() {
 	Route::get('/test', function(){return "it works";});
 	Route::get('/test2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
 	Route::get('/students', 'sp2gr11\reservation\controllers\ReservationController@getStudent');
-	Route::post('/students', 'sp2gr11\reservation\controllers\ReservationController@postReservationRequest');
+	Route::post('/postrequestreservation', 'sp2gr11\reservation\controllers\ReservationController@postReservationRequest');
+	Route::get('/initdoc', 'sp2gr11\reservation\controllers\ReservationController@getAssetIDs');
 });
