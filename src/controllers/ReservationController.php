382b602ecbbc182355860bba19a1f148480ad892
DESKTOP-TRISTAN\Tristan <tristan.bomans@student.ehb.be>
2016-11-26 21:08:41 +0100

    Added a lending service view with some basic check-in & check-out functionality. (Fine etc isn't being calculated yet)

--- src/controllers/ReservationController.php
+++ src/controllers/ReservationController.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\DB;
 
 class ReservationController extends Controller
 {
+
+	// STILL NEED TO CHECK FOR EVERY FUNCTION THE USERS AUTHORITY (e.g. STUDENT CANT ACCEPT/REJECT REQUESTS(OR EVEN SEE THAT PAGE FOR THAT MATTER))
+
 	//private functions
 
 
@@ -22,6 +25,8 @@ class ReservationController extends Controller
 	{
 		$this->middleware('auth');
 	}
+
+	// (!!FOR NOW!!) UNUSED FUNCTIONS
 
 	public function getReservation()
 	{
@@ -45,74 +50,8 @@ class ReservationController extends Controller
 			['leasedAssets' => ReservationFetcher::getLeasedAssets()],
 		]); // will add more variables to send
 	}
-
-
-	public function postReservationRequest(Request $request)
-	{
-	///__________________DE INSERT_____________________________________
-		if(DB::table('requested_assets')->insert([
-	        'user_id'=> Auth::id(),
-    	    'asset_id' => $request->asset_id 
-        ])){
-			$worked = true;
-		}
-		else{
-			$worked = false;
-		}
-
-	///__________________INSERTED VALUE RETURNEN___________________________________
-		
-		if($worked == true){
-   			$user_requested_assets = self::getUserAssets();
-			$temp = sizeof($user_requested_assets);
-			$temp = $temp - 1;
-			$inserted_user = $user_requested_assets[$temp];
-
-			$newarray = Array();
-
-			$newarray[0] = $inserted_user->user_id;
-			$newarray[1] = $inserted_user->asset_id;
-			$newarray[2] = $inserted_user->name;
-			$newarray[3] = $inserted_user->Aname;
-			
-			return $newarray;
- 	  	}
- 		return false;
-	}
-
-
-	public function rejectedReservation(Request $request)
-	{
-		$to_delete_request_asset_id = $request->req_asset_id;
-		$to_delete_request_user_id = $request->req_user_id;
-		DB::table('requested_assets')
-		->where([['user_id','=',$to_delete_request_user_id],['asset_id','=',$to_delete_request_asset_id]])
-		->delete();
-
-		return "succes";
-
-	}
-	public function postReservation(Request $request)
-	{	
-		$to_submit_request_id = $request->req_asset_id;
-		
-		// if (!RoleUtil::isUserLeasingService()){
-		// 	return redirect()->back();
-		// }
-		// else{
-		//var_dump($to_submit_request_id);
-
-		DB::table('reservation_assets')->insert([
-		        'user_id'=> 1,//$request->input('user_id'),
-        	    'asset_id' => $to_submit_request_id, //,
-        	    'from' => date('Y-m-d', strtotime($request->input('from'))),
-           		'until' => date('Y-m-d', strtotime($request->input('until')))  
-            ]);
-
-		DB::table('requested_assets')->where('asset_id',$to_submit_request_id)->delete();
-
-		return "succes";
-	}
+	
+	// FUNCTIONS WHO RETURN VIEWS
 
 	public function getIndex(){
 		return "Reaching the controllers works aswell";
@@ -121,6 +60,16 @@ class ReservationController extends Controller
 	public function getStudent(){
 		return view('reservation::students')->with('assets', self::getFreeAssets())->with('userassets',self::getUserAssets());
 	}
+
+	public function getProfessor(){
+		return view('reservation::professors')->with('requestedassets', self::getRequestedAssets());
+	}
+	public function getLservice(){
+		return view('reservation::lendingservice')->with('assets', self::getFreeAssets());
+	}
+
+	// DB INTERACTIONFUNCTIONS, HAS TO BE MOVED TO FETCHERS IN FUTURE
+	// GET FUNCTIONS
 
 	public function getFreeAssets() {
 
@@ -167,8 +116,39 @@ class ReservationController extends Controller
 		return $free_asset_name_id;
 	}
 
-	public function getProfessor(){
-		return view('reservation::professors')->with('requestedassets', self::getRequestedAssets());
+	public function getAllinfoLS(){
+
+		$free_assets =  Asset::all();
+		$free_asset_combo = Array();
+		$free_asset_name_id = Array();
+
+		$all_users = User::all();
+		$free_users_combo = Array();
+		$free_users_name_id = Array();
+
+		$users_asset_combo = Array();
+
+		for ($i=0; $i <sizeof($free_assets) ; $i++) { 
+
+			unset($free_asset_combo);
+			$free_asset_combo = Array();
+			array_push($free_asset_combo, $free_assets[$i]["id"], $free_assets[$i]["name"]);
+			array_push($free_asset_name_id, $free_asset_combo);
+
+		}
+
+		for ($i=0; $i <sizeof($all_users) ; $i++){ 
+
+			unset($free_users_combo);
+			$free_users_combo = Array();
+			array_push($free_users_combo, $all_users[$i]["id"], $all_users[$i]["first_name"]." ".$all_users[$i]["last_name"]);
+			array_push($free_users_name_id, $free_users_combo);
+		}
+
+		$return_array = Array();
+		array_push($return_array, $free_asset_name_id, $free_users_name_id);
+
+		return $return_array;
 	}
 
 	public function getRequestedAssets(){
@@ -176,14 +156,136 @@ class ReservationController extends Controller
 		$reservation_requests = DB::table('requested_assets')->get();
 
 		for ($i=0; $i <sizeof($reservation_requests); $i++) { 
-			
+
 			$user = User::where('id', $reservation_requests[$i]->user_id)->get()[0];
 			$asset = Asset::where('id', $reservation_requests[$i]->asset_id)->get()[0]["name"];
 
     		$reservation_requests[$i]->name = $user["first_name"]." ".$user["last_name"];
 			$reservation_requests[$i]->Aname = $asset;
-
 		}
 		return $reservation_requests;
 	}
+
+	// POST FUNCTIONS
+
+	public function postReservation(Request $request)
+	{	
+		$to_submit_request_id = $request->req_asset_id;
+		
+		// if (!RoleUtil::isUserLeasingService()){
+		// 	return redirect()->back();
+		// }
+		// else{
+		//var_dump($to_submit_request_id);
+
+		DB::table('reservation_assets')->insert([
+		        'user_id'=> 1,//$request->input('user_id'),
+        	    'asset_id' => $to_submit_request_id, //,
+        	    'from' => date('Y-m-d', strtotime($request->input('from'))), //THESE TWO DATA FUNCTIONS ARE USELESS (INSERT GIBBERISH VALUES, NEED TO BE CHANGED)
+           		'until' => date('Y-m-d', strtotime($request->input('until')))  
+            ]);
+
+		DB::table('requested_assets')->where('asset_id',$to_submit_request_id)->delete();
+
+		return "succes";
+	}
+
+
+	public function postReservationRequest(Request $request){
+		
+	///__________________THE INSERT_____________________________________
+
+		if(DB::table('requested_assets')->insert([
+	        'user_id'=> Auth::id(),
+    	    'asset_id' => $request->asset_id 
+        ])){
+			$worked = true;
+		}
+		else{
+			$worked = false;
+		}
+
+	///__________________ RETURN INSERTED VALUE ___________________________________
+		
+		if($worked == true){
+   			$user_requested_assets = self::getUserAssets();
+			$temp = sizeof($user_requested_assets);
+			$temp = $temp - 1;
+			$inserted_user = $user_requested_assets[$temp];
+
+			$newarray = Array();
+
+			$newarray[0] = $inserted_user->user_id;
+			$newarray[1] = $inserted_user->asset_id;
+			$newarray[2] = $inserted_user->name;
+			$newarray[3] = $inserted_user->Aname;
+			
+			return $newarray;
+ 	  	}
+ 		return false;
+	}
+
+	public function lsaction(Request $request){
+
+		$asset_id = $request->asset_id;
+		$action =  $request->asset_action;
+		$user_id = $request->user_id;
+
+		$status = DB::table('assets')->where('id', $asset_id)->get()[0]->assigned_to;
+
+		if($action == "checkout"){
+			
+			if (!$status == null){return "ERROR: ASSET ISN'T AVAILABLE!";} 
+
+			DB::table('assets')
+            ->where('id', $asset_id)
+            ->update(['assigned_to' => $user_id]);
+
+            return "Succesfully assigned asset with ID ". $asset_id." to user with ID ". $user_id;
+		}
+		elseif ($action == "checkin") {
+
+			if ($status == null){return "ERROR: ASSET IS ALREADY IN THE INVENTORY";} 
+
+			DB::table('assets')
+            ->where('id', $asset_id)
+            ->update(['assigned_to' => null]);
+
+           return "Succesfully checked asset with ID ". $asset_id." in.";
+			
+		}
+		elseif ($action == "overtime") {
+
+			if ($status == null){return "ERROR: ASSET IS ALREADY IN THE INVENTORY";} 
+
+			DB::table('assets')
+            ->where('id', $asset_id)
+            ->update(['assigned_to' => null]);
+
+			//INSERT FINE CALC OR SMTN
+
+			return "Succesfully checked asset with ID ". $asset_id." in. The asset was too late, fine will be sent";
+		}
+		else{
+
+			return "select a legitimate action!";
+
+		}
+
+	}
+
+	// THIS FUNCTION IS FOR DELETING RESERVATION OUT OF REQUESTS, STILL NEED TO ARCHIVE
+
+	public function rejectedReservation(Request $request)
+	{
+		$to_delete_request_asset_id = $request->req_asset_id;
+		$to_delete_request_user_id = $request->req_user_id;
+
+		DB::table('requested_assets')
+		->where([['user_id','=',$to_delete_request_user_id],['asset_id','=',$to_delete_request_asset_id]])
+		->delete();
+
+		return "succes";
+	}
+
 }
--- src/routes/routes.php
+++ src/routes/routes.php
@@ -1,12 +1,22 @@
 <?php
 Route::group([ 'prefix'  => 'package' ], function() {
+
+	// TESTS: ROUTE TEST & CONTROLLER TEST
 	Route::get('/test', function(){return "it works";});
 	Route::get('/test2', 'sp2gr11\reservation\controllers\ReservationController@getIndex');
+
+	// MAIN ROUTES
 	Route::get('/students', 'sp2gr11\reservation\controllers\ReservationController@getStudent');
 	Route::get('/professors', 'sp2gr11\reservation\controllers\ReservationController@getProfessor');
-	Route::get('/postrequestreservation', 'sp2gr11\reservation\controllers\ReservationController@postReservationRequest');
+	Route::get('/lendingservice', 'sp2gr11\reservation\controllers\ReservationController@getLservice');
+
+	// USED BY AJAX CALLS
+	Route::get('/lsaction', 'sp2gr11\reservation\controllers\ReservationController@lsaction');
 	Route::get('/initdoc', 'sp2gr11\reservation\controllers\ReservationController@getAssetIDandNames');
+	Route::get('/initdoclendservice', 'sp2gr11\reservation\controllers\ReservationController@getAllinfoLS');		
 	Route::get('/postreservation', 'sp2gr11\reservation\controllers\ReservationController@postreservation');
 	Route::get('/rejectreservation', 'sp2gr11\reservation\controllers\ReservationController@rejectedReservation');
+	Route::get('/postrequestreservation', 'sp2gr11\reservation\controllers\ReservationController@postReservationRequest');
+
 
 });
