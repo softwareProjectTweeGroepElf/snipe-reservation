e08a55d637e8fa5f2a43f32d7a7a860457de7c9e
Sam-PC\Sam <sam.van.roy@student.ehb.be>
2016-11-29 19:43:42 +0100

    tijdelijke toevoeging andere mailFuncties + inkorten van functienamen

--- src/util/MailUtil.php
+++ src/util/MailUtil.php
@@ -38,4 +38,92 @@ class MailUtil
         });
 
     }
+
+    public static function sendResultDecisionTeacher($decision, $reservation){
+        $student_id = $reservation->user_id;
+        $student = User::find($student_id);
+        $student_asset_id = $reservation->asset_id;
+        $student_asset = DB::table('assets')->where('id', $student_asset_id);
+
+        $data = array();
+        $data['first_name'] = $student->first_name;
+        $data['last_name'] = $student->last_name;
+        $data['asset_name'] = $student_asset->name;
+        if($decision)
+        {
+            $data['decision'] = 'accepted';
+        }
+        else {
+            $data['decision'] = 'denied';
+        }
+        Mail::send('emails.resultDecisionTeacher', $data ,function ($m) use ($student) {
+            $m->to($student->email, $student->first_name . ' ' . $student->last_name);
+            $m->subject('Decision teacher about your asset');
+        });
+
+    }
+
+    public function sendReminderMail(){
+        $reservations = $this->getAllReservations1DayBeforeEndDate();
+        $data = array();
+        foreach ($reservations as $reservation) {
+            $user_id = $reservation->user_id;
+            $user = User::find($user_id);
+            $user_asset_id = $reservation->asset_id;
+            $user_asset = DB::table('assets')->where('id', $user_asset_id);
+
+            $data['first_name'] = $user->first_name;
+            $data['last_name'] = $user->last_name;
+            $data['asset_name'] = $user_asset->name;
+
+            Mail::send('emails.reminderMailUser', $data, function ($m) use ($user) {
+                $m->to($user->email, $user->first_name . ' ' . $user->last_name);
+                $m->subject('Automatic reminder ending loan period asset');
+            });
+        }
+    }
+
+    //2e herinnering voor te zeggen dat het te laat is & dat er een boete volgt.
+
+    public function sendSecondReminderMail(){
+        $reservations = $this->getAllEndDateReservations();
+        $data = array();
+        foreach ($reservations as $reservation) {
+            $user_id = $reservation->user_id;
+            $user = User::find($user_id);
+            $user_asset_id = $reservation->asset_id;
+            $user_asset = DB::table('assets')->where('id', $user_asset_id);
+
+            $data['first_name'] = $user->first_name;
+            $data['last_name'] = $user->last_name;
+            $data['asset_name'] = $user_asset->name;
+
+            Mail::send('emails.secondReminderMailUser', $data, function ($m) use ($user) {
+                $m->to($user->email, $user->first_name . ' ' . $user->last_name);
+                $m->subject('Automatic reminder loan period ended!');
+            });
+        }
+    }
+
+//De student zal een email ontvangen dat zijn gereserveerde item klaar ligt op de uitleendienst.
+    public function sendEmailWhenAssetIsLendable(){
+        $reservations = $this->getAllAssetsReadyForLoaning();
+        $data = array();
+
+        foreach ($reservations as $reservation) {
+            $user_id = $reservation->user_id;
+            $user = User::find($user_id);
+            $user_asset_id = $reservation->asset_id;
+            $user_asset = DB::table('assets')->where('id', $user_asset_id);
+
+            $data['first_name'] = $user->first_name;
+            $data['last_name'] = $user->last_name;
+            $data['asset_name'] = $user_asset->name;
+
+            Mail::send('emails.assetIsReadyForLoan', $data, function ($m) use ($user) {
+                $m->to($user->email, $user->first_name . ' ' . $user->last_name);
+                $m->subject('Your asset is ready for loan!');
+            });
+        }
+    }
 }
