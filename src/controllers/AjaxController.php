782b0200f4fd0215869553b567494734c573f365
lupyx <tanguy.deleu@student.ehb.be>
2016-12-13 16:52:17 +0100

    Added getAssetReservationsForMonth

--- src/controllers/AjaxController.php
+++ src/controllers/AjaxController.php
@@ -30,28 +30,30 @@ class AjaxController extends Controller
         $this->reservation_util = $reservation_util;
         $this->reservation_fetcher = $reservation_fetcher;
     }
+
     /*
      * Lending Service AJAX
      */
-    public function lsaction(Request $request, CheckOutUtil $check_out_util, CheckInUtil $check_in_util, FineUtil $fine_util){
+    public function lsaction(
+        Request $request,
+        CheckOutUtil $check_out_util,
+        CheckInUtil $check_in_util,
+        FineUtil $fine_util
+    ) {
         $asset_id = $request->asset_id;
-        $action =  $request->asset_action;
+        $action = $request->asset_action;
 
 
-        switch($action)
-        {
-            case 'checkout':
-            {
+        switch ($action) {
+            case 'checkout': {
                 $check_out_util->checkOutByAssetId($asset_id);
                 break;
             }
-            case 'checkin':
-            {
+            case 'checkin': {
                 $check_in_util->checkInByAssetId($asset_id);
                 break;
             }
-            case 'overtime':
-            {
+            case 'overtime': {
                 $fine_util->fine($check_in_util->checkInByAssetId($asset_id));
                 break;
             }
@@ -60,7 +62,8 @@ class AjaxController extends Controller
         return "Select a legitimate action!";
     }
 
-    public function getAllinfoLS(){
+    public function getAllinfoLS()
+    {
         $available_assets = $this->reservation_fetcher->getAvailableAssets();
         $lent_assets = $this->reservation_fetcher->getLeasedAssets();
         $users = User::all();
@@ -68,14 +71,17 @@ class AjaxController extends Controller
 
         $asset_data = array();
 
-        foreach($available_assets as $asset)
+        foreach ($available_assets as $asset) {
             array_push($asset_data, $asset->id, $asset->name);
+        }
 
-        foreach($lent_assets as $asset)
+        foreach ($lent_assets as $asset) {
             array_push($asset_data, $asset->id, $asset->name);
+        }
 
-        foreach ($users as $user)
-            array_push($free_users_combo, $user->id, $user->first_name." ". $user->last_name);
+        foreach ($users as $user) {
+            array_push($free_users_combo, $user->id, $user->first_name . " " . $user->last_name);
+        }
 
         return ['assets' => $asset_data, 'users' => $free_users_combo];
     }
@@ -88,13 +94,15 @@ class AjaxController extends Controller
         $free_assets = $this->reservation_fetcher->getAvailableAssets();
         $data = array();
 
-        foreach($free_assets as $asset)
+        foreach ($free_assets as $asset) {
             array_push($data, ['id' => $asset->id, 'name' => $asset->name]);
+        }
 
         return $data;
     }
 
-    public function postReservationRequest(Request $request){
+    public function postReservationRequest(Request $request)
+    {
         $this->reservation_util->createReservationRequest(Auth::user()->id, $request->asset_id);
         return true;
     }
@@ -104,16 +112,28 @@ class AjaxController extends Controller
      */
     public function postReservation(Request $request)
     {
-        $reservation_id = $this->reservation_util->getRequestIdForUserAsset($request->req_asset_id, $request->req_user_id);
+        $reservation_id = $this->reservation_util->getRequestIdForUserAsset($request->req_asset_id,
+            $request->req_user_id);
         $this->reservation_util->acceptReservation($reservation_id);
     }
 
     public function rejectedReservation(Request $request)
     {
-        $this->reservation_util->rejectReservation($this->reservation_util->getRequestIdForUserAsset($request->req_asset_id, $request->req_user_id));
+        $this->reservation_util->rejectReservation($this->reservation_util->getRequestIdForUserAsset($request->req_asset_id,
+            $request->req_user_id));
     }
+
     public function getLeasedAssetsExceptOvertime()
     {
         return $this->reservation_fetcher->getLeasedAssetsExceptOvertime();
+    }
+
+    /*
+     * Calendar
+     */
+
+    public function getAssetReservationsForMonth($asset_id, $month)
+    {
+        return $this->reservation_fetcher->getReservationsForMonth($asset_id, $month);
     }
 }
