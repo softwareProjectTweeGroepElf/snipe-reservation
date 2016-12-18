56f4cf819a6c95e561c4774360a3d82d9918653c
lupyx <tanguy.deleu@student.ehb.be>
2016-12-18 21:28:28 +0100

    Added some columns to migration to store extra details

--- src/migrations/2016_11_20_132343_create_reservation_requests_table.php
+++ src/migrations/2016_11_20_132343_create_reservation_requests_table.php
@@ -20,6 +20,8 @@ class CreateReservationRequestsTable extends Migration
             $table->integer('user_id');
             $table->integer('asset_id');
             $table->string('subject', 40);
+            $table->string('note');
+            $table->dateTime('preferred_date');
             $table->timestamps();
         });
     }
