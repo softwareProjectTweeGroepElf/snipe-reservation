6c732d03ad341c0e67fd10d8e56cd63099f074f6
lupyx <tanguy.deleu@student.ehb.be>
2016-12-21 16:01:08 +0100

    Added search to Request reservation page

--- src/resources/views/requestreservation.blade.php
+++ src/resources/views/requestreservation.blade.php
@@ -1,6 +1,10 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script>
 	$(document).ready(function() {
+		$('#search').change(function() {
+			searchAvailableAssets();
+		});
+		
 		$.ajax({
 			type: 'GET',
 			url: '/reservation/initdoc',
@@ -23,6 +27,23 @@
 			success: function(data) {
 				console.log(data);
 				location.reload();
+			}
+		});
+	}
+	
+	function searchAvailableAssets()
+	{
+		var search_text = $('#search').val();
+		$.ajax({
+			type: 'GET',
+			url: '/reservation/searchAvailableAssets',
+			data: { 'text': search_text, 'filter': 'name' },
+			success: function(data) {
+				$('#search_content tr:not(:first)').remove();
+				
+				for(var i = 0; i < data.length; i++) {
+					$('#search_content').append('<tr><td>' + data[i]['name'] + '</td></tr>');
+				}
 			}
 		});
 	}
@@ -176,7 +197,7 @@
 			</div>
 			<hr>
 			
-			<h1>List of your requests: </h1>
+			<h1>Uw aanvragen: </h1>
 			
 			<div class="requested_assets_table_cont">
 				<div class="requested_assets_table_cont2">
@@ -200,6 +221,21 @@
 				</div>
 			</div>
 			<hr>
+			
+			<h1>Beschikbare voorwerpen: </h1>
+			<input type="text" id="search" placeholder="Zoeken..">
+			<div class="requested_assets_table_cont">
+				<div class="requested_assets_table_cont2">
+					<table class="requested_assets_table" id="search_content">
+						<tr>
+							<th>Voorwerp</th>
+						</tr>
+						
+						@foreach($assets as $asset)
+							<tr>
+								<td> {{$asset->name}} </td>
+							</tr>
+						@endforeach
 		</div>
 	</div>
 	
